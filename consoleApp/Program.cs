7769598fea4d3852719b348ad863ca798b0ea0e3
Arabela08 <delacruzarabelagrace3@gmail.com>
2025-07-30 01:39:21 +0000

    saur good

---
+++ consoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Arabela Grace Y. Dela Cruz");
+        Console.WriteLine("20221118645");
+        Console.WriteLine("saur good, saur great");
+    }    
+} 
